2fd61c5f1a96388bfb5bc6f7abcf4306cbde7f5c
Walker Jan (s) <jan.walker@students.fhnw.ch>
2018-01-17 19:34:04 +0100

    formatted the code

--- module5-solution/js/script.js
+++ module5-solution/js/script.js
@@ -118,10 +118,10 @@ $(function() { // Same as document.addEventListener("DOMContentLoaded"...
         var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml, "randomCategoryShortName", chosenCategoryShortName);
 
 
-          // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
-          // Use the existing insertHtml function for that purpose. Look through this code for an example
-          // of how to do that.
-          insertHtml("#main-content", homeHtmlToInsertIntoMainPage);
+        // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
+        // Use the existing insertHtml function for that purpose. Look through this code for an example
+        // of how to do that.
+        insertHtml("#main-content", homeHtmlToInsertIntoMainPage);
 
       },
       false); // False here because we are getting just regular HTML from the server, so no need to process JSON.
